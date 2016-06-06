c88a26215d570d0a5d338d4071b7d27fd374b396
Guus <guus_waals@live.nl>
2016-06-06 19:45:54 +0200

    Added CMake to project

--- ProjectTemplates/AppTemplate/AppTemplate.cpp
+++
@@ -1,6 +0,0 @@
-#include "stdafx.h"
-
-int32 __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
-{
-	return 0;
-}
--- ProjectTemplates/AppTemplate/stdafx.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// Include shared library
-#include <Shared/Shared.hpp>
