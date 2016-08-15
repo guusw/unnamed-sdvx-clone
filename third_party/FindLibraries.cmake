7cdd331dc72130aa50b2fbcb5253542fa24883b3
Guus <guus_waals@live.nl>
2016-08-15 15:36:32 +0200

    Added FindOGG and FindVorbis

--- third_party/FindLibraries.cmake
+++ third_party/FindLibraries.cmake
@@ -4,9 +4,13 @@ find_package(ZLIB REQUIRED)
 find_package(SDL2 REQUIRED)
 find_package(PNG REQUIRED)
 find_package(JPEG REQUIRED)
+find_package(Vorbis REQUIRED)
+find_package(OGG REQUIRED)
 
 # Linux include directories
 include_directories(
 	${FREETYPE_INCLUDE_DIRS}
 	${ZLIB_INCLUDE_DIRS}
-	${SDL2_INCLUDE_DIR})
+	${SDL2_INCLUDE_DIR}
+	${VORBIS_INCLUDE_DIR}
+	${OGG_INCLUDE_DIR})
